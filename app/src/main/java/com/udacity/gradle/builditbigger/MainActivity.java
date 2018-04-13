67bc04a4badd38ab4021237b17848472f12b7fd9
Natalia Jastrzębska <nataliak@10g.pl>
2018-04-13 12:20:29 +0200

    displaying joke inside android lib

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -7,6 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import eu.napcode.displayjoke.DisplayJokeActivity;
 import eu.napcode.javajokes.Joker;
 
 
@@ -42,6 +44,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, Joker.getJoke(), Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, DisplayJokeActivity.class);
+        intent.putExtra(DisplayJokeActivity.JOKE_KEY, Joker.getJoke());
+
+        startActivity(intent);
     }
 }
